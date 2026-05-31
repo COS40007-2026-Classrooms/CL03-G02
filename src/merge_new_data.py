f356dbbaeba7e9d35030f7966996b222c7a640ef
biggiebrooms <kai.broomfield@icloud.com>
2026-05-31 22:35:11 +1000

    fix yml retrain and added merge script

---
+++ src/merge_new_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+new = pd.read_csv('data/new_data.csv')
+train = pd.read_csv('train/train.csv')
+merged = pd.concat([train, new], ignore_index=True)
+merged.to_csv('train/train.csv', index=False)
+print(f'Merged {len(new)} new rows into train.csv (total: {len(merged)})')
